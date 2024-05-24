b83b0ac654ce4b4851606d9263c4e0b77b42d2c9
Nikita <nik.pogrebnoy@gmail.com>
2024-05-24 14:38:54 +0300

    Replaced the existing BookRepository with the one that will use JpaRepository interface Added Liquibase support Changed spring.jpa.hibernate.ddl-auto property value to validate Implemented soft delete concept for the Book entity

--- src/main/java/mate/academy/model/Book.java
+++ src/main/java/mate/academy/model/Book.java
@@ -9,10 +9,14 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Getter
 @Setter
 @Entity
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
 @Table(name = "books")
 public class Book {
     @Id
@@ -29,4 +33,6 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    @Column(nullable = false)
+    private Boolean isDeleted = false;
 }
