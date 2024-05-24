0bd93d1b631ab52576db4243316cd2f1b852ba36
Nikita <149311860+ynik404@users.noreply.github.com>
2024-05-24 11:04:49 +0300

    Spring boot web solution (#7)
    * added Book entity,
    added BookRepository interface with methods and BookRepositoryImpl class,
    added BookService interface with methods and BookServiceImpl class
    
    * implemented spring web intro
    
    * implemented spring web intro
    
    * implemented spring web intro
    
    * implemented mapstruct
    
    * fixed book entity
    
    * added equals and hashcode to book
    
    * added annotations
    
    * fixed book
    added and implemented DataProcessingException

--- src/main/java/mate/academy/model/Book.java
+++ src/main/java/mate/academy/model/Book.java
@@ -5,11 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
-@Data
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/mate/academy/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/service/impl/BookServiceImpl.java
@@ -1,24 +1,38 @@
 package mate.academy.service.impl;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.dto.BookDto;
+import mate.academy.dto.CreateBookRequestDto;
+import mate.academy.mapper.BookMapper;
 import mate.academy.model.Book;
 import mate.academy.repository.BookRepository;
 import mate.academy.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto createBook(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        Book createdBook = bookRepository.createBook(book);
+        return bookMapper.toDto(createdBook);
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public BookDto getBookById(long id) {
+        Book bookById = bookRepository.getBookById(id);
+        return bookMapper.toDto(bookById);
+    }
+
+    @Override
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
     }
 }
